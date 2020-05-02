0683aca0e3efc20d6dbcf77bdace97e9fc0ec073
Ursu Marian-Bogdan <ursu.marianbogdan@gmail.com>
2020-05-02 22:54:33 +0300

    Updates

---
+++ precache-manifest.0ce7fd8e36dda3ae2141433f6b401e7a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2265d5748afd0130f3d486984dc72a5d",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "081513199934914e2754",
+    "url": "/facerecognition/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "a51cbf92a9b07b5079e0",
+    "url": "/facerecognition/static/css/main.4706fe44.chunk.css"
+  },
+  {
+    "revision": "081513199934914e2754",
+    "url": "/facerecognition/static/js/2.18d3b627.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognition/static/js/2.18d3b627.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a51cbf92a9b07b5079e0",
+    "url": "/facerecognition/static/js/main.82749f65.chunk.js"
+  },
+  {
+    "revision": "b1c0e8aee0d06b10d4f1",
+    "url": "/facerecognition/static/js/runtime-main.b5d4f63f.js"
+  },
+  {
+    "revision": "fdd3106c13fef9151b06e2e5c73f4e79",
+    "url": "/facerecognition/static/media/emoji.fdd3106c.png"
+  }
+]);
